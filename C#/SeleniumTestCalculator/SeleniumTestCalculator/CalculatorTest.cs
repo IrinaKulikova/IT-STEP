5ed09641ef25685d69234f9b3bd76355ead78543
Irina <cg.irenecooper@gmail.com>
2018-11-14 21:48:18 +0200

    fix

--- C#/SeleniumTestCalculator/SeleniumTestCalculator/CalculatorTest.cs
+++ C#/SeleniumTestCalculator/SeleniumTestCalculator/CalculatorTest.cs
@@ -4,200 +4,100 @@ using static Xunit.Assert;
 
 namespace SeleniumTestCalculator
 {
-    //Написать тесткейсы для тестирования калькулятора с применением Selenium WebDriver
+    //Написать тесткейсы для тестирования калькулятора с приминением Selenium WebDriver
     public class CalculatorTest : BaseTest
     {
-
-        [Fact]
-        public void TestPlus()
+        [Theory]
+        [InlineData("0", "+", "0", "0")]
+        [InlineData("1", "+", "0", "1")]
+        [InlineData("1", "+", "1", "2")]
+        [InlineData("1", "+", "2", "3")]
+        [InlineData("1", "+", "3", "4")]
+        [InlineData("1", "+", "4", "5")]
+        [InlineData("1", "+", "5", "6")]
+        [InlineData("1", "+", "6", "7")]
+        [InlineData("1", "+", "7", "8")]
+        [InlineData("1", "+", "8", "9")]
+        [InlineData("1", "+", "9", "10")]
+        [InlineData("0", "-", "0", "0")]
+        [InlineData("9", "-", "0", "9")]
+        [InlineData("9", "-", "9", "0")]
+        [InlineData("9", "-", "8", "1")]
+        [InlineData("9", "-", "7", "2")]
+        [InlineData("9", "-", "6", "3")]
+        [InlineData("9", "-", "5", "4")]
+        [InlineData("9", "-", "4", "5")]
+        [InlineData("9", "-", "3", "6")]
+        [InlineData("9", "-", "2", "7")]
+        [InlineData("9", "-", "1", "8")]
+        [InlineData("0", "x", "0", "0")]
+        [InlineData("2", "x", "1", "2")]
+        [InlineData("2", "x", "2", "4")]
+        [InlineData("2", "x", "3", "6")]
+        [InlineData("2", "x", "4", "8")]
+        [InlineData("2", "x", "5", "10")]
+        [InlineData("2", "x", "6", "12")]
+        [InlineData("2", "x", "7", "14")]
+        [InlineData("2", "x", "8", "16")]
+        [InlineData("2", "x", "9", "18")]
+        [InlineData("0", "/", "0", "0")]
+        [InlineData("2", "/", "1", "2")]
+        [InlineData("2", "/", "2", "1")]
+        [InlineData("2", "/", "4", "0.5")]
+        [InlineData("2", "/", "5", "0,4")]
+        [InlineData("2", "/", "8", "0,25")]
+        public void TestAllNaturalNumbers(string arg1, string operation, string arg2, string result)
         {
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            webDriver.FindElement(By.CssSelector(controls[arg1])).Click();
+            Equal(arg1, webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
 
-            webDriver.FindElement(By.CssSelector(controls["+"])).Click();
-            Equal("2+", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            webDriver.FindElement(By.CssSelector(controls[operation])).Click();
+            Equal(arg1 + operation, webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
 
-            webDriver.FindElement(By.CssSelector(controls["4"])).Click();
-            Equal("2+4", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            webDriver.FindElement(By.CssSelector(controls[arg2])).Click();
+            Equal(arg1 + operation + arg2, webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
 
             webDriver.FindElement(By.CssSelector(controls["="])).Click();
-            Equal("6", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            Equal(result, webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
         }
 
-        [Fact]
-        public void TestMinus()
+
+        [Theory]
+        [InlineData("0", "2", "+", "0", "1", "0.3")]
+        [InlineData("0", "2", "-", "0", "1", "0.1")]
+        [InlineData("2", "2", "+", "0", "1", "2.3")]
+        [InlineData("8", "2", "+", "9", "3", "-1.1")]
+        [InlineData("0", "2", "x", "5", "5", "1.1")]
+        [InlineData("0", "2", "/", "0", "1", "2")]
+        [InlineData("5", "5", "+", "2", "2", "12.1")]
+        [InlineData("1", "0", "x", "1", "3", "1.3")]
+        [InlineData("12", "2", "/", "0", "0", "Infinity")]
+
+        public void TestAllRealNumbers(string arg11, string arg12, string operation, string arg21, string arg22, string result)
         {
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["-"])).Click();
-            Equal("2-", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["4"])).Click();
-            Equal("2-4", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["="])).Click();
-            Equal("-2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-        }
-
-        [Fact]
-        public void TestMulti()
-        {
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["x"])).Click();
-            Equal("2x", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["4"])).Click();
-            Equal("2x4", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["="])).Click();
-            Equal("8", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-        }
-
-        [Fact]
-        public void TestDiv()
-        {
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["/"])).Click();
-            Equal("2/", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["1"])).Click();
-            Equal("2/1", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["="])).Click();
-            Equal("2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-        }
-
-        [Fact]
-        public void TestDivNull()
-        {
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["/"])).Click();
-            Equal("2/", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["0"])).Click();
-            Equal("2/0", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["="])).Click();
-            Equal("Infinity", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-        }
-
-        [Fact]
-        public void TestPlusRealNumbers()
-        {
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            webDriver.FindElement(By.CssSelector(controls[arg11])).Click();
+            Equal(arg11, webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
 
             webDriver.FindElement(By.CssSelector(controls["."])).Click();
-            Equal("2.", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            Equal(arg11 + ".", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
 
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2.2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            webDriver.FindElement(By.CssSelector(controls[arg12])).Click();
+            Equal(arg11 + "." + arg12, webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
 
-            webDriver.FindElement(By.CssSelector(controls["+"])).Click();
-            Equal("2.2+", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            webDriver.FindElement(By.CssSelector(controls[operation])).Click();
+            Equal(arg11 + "." + arg12 + operation, webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
 
-            webDriver.FindElement(By.CssSelector(controls["4"])).Click();
-            Equal("2.2+4", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            webDriver.FindElement(By.CssSelector(controls[arg21])).Click();
+            Equal(arg11 + "." + arg12 + operation + arg21, webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
 
             webDriver.FindElement(By.CssSelector(controls["."])).Click();
-            Equal("2.2+4.", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            Equal(arg11 + "." + arg12 + operation + arg21 + ".", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
 
-            webDriver.FindElement(By.CssSelector(controls["4"])).Click();
-            Equal("2.2+4.4", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            webDriver.FindElement(By.CssSelector(controls[arg22])).Click();
+            Equal(arg11 + "." + arg12 + operation + arg21 + "." + arg22, webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
 
             webDriver.FindElement(By.CssSelector(controls["="])).Click();
-            Equal("6.6", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-        }
-
-        [Fact]
-        public void TestMinusRealNumbers()
-        {
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["."])).Click();
-            Equal("2.", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2.2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["-"])).Click();
-            Equal("2.2-", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["4"])).Click();
-            Equal("2.2-4", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["."])).Click();
-            Equal("2.2-4.", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["4"])).Click();
-            Equal("2.2-4.4", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["="])).Click();
-            Equal("-2.2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-        }
-
-        [Fact]
-        public void TestMultiRealNumbers()
-        {
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["."])).Click();
-            Equal("2.", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2.2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["x"])).Click();
-            Equal("2.2x", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["5"])).Click();
-            Equal("2.2x5", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["."])).Click();
-            Equal("2.2x5.", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["5"])).Click();
-            Equal("2.2x5.5", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["="])).Click();
-            Equal("12.1", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-        }
-
-        [Fact]
-        public void TestDivRealNumbers()
-        {
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["."])).Click();
-            Equal("2.", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["2"])).Click();
-            Equal("2.2", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["/"])).Click();
-            Equal("2.2/", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["5"])).Click();
-            Equal("2.2/5", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["."])).Click();
-            Equal("2.2/5.", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["5"])).Click();
-            Equal("2.2/5.5", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
-
-            webDriver.FindElement(By.CssSelector(controls["="])).Click();
-            Equal("0.4", webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
+            Equal(result, webDriver.FindElement(By.CssSelector(controls["screen"])).Text);
         }
     }
 }
